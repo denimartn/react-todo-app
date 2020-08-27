ea26b6bdafc8adee89a5e9ba5dd4cb482b3e773e
denimartn <43113570+denimartn@users.noreply.github.com>
2020-08-27 16:58:53 +0200

    add template and style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [inputValue, setInputValue] = React.useState("");
+
+  const [todos, setTodos] = React.useState([]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue.length === 0) {
+      return;
+    }
+    addTodo();
+    setInputValue("");
+  };
+  const addTodo = () => {
+    setTodos([
+      ...todos,
+      {
+        id: Math.random(),
+        value: inputValue,
+        isDone: false,
+      },
+    ]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="row">
+      <div className="row2">
+        <h1>#todo</h1>
+        <div className="options">
+          <button className="active" id="all" type="button">
+            All
+          </button>
+          <button id="active" type="button">
+            Active
+          </button>
+          <button id="completed" type="button">
+            Completed
+          </button>
+        </div>
+        <form onSubmit={onSubmit}>
+          <div className="input-wrapper">
+            <input
+              className="input-text"
+              type="text"
+              value={inputValue}
+              onChange={(event) => setInputValue(event.target.value)}
+              placeholder="Type something..."
+            />
+
+            <button id="add" type="submit">
+              Add
+            </button>
+          </div>
+          <div className="list-wrapper">
+            <ul>
+              {todos.map((todo) => (
+                <li key={todo.id}>
+                  <input type="checkbox"></input>
+                  {todo.value}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </form>
+      </div>
+    </main>
   );
-}
+};
 
 export default App;
