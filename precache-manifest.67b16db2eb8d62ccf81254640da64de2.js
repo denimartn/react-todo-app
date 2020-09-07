dda188050c7e68e62e0200676854e70327e30f26
denimartn <43113570+denimartn@users.noreply.github.com>
2020-09-07 15:02:52 +0200

    Updates

---
+++ precache-manifest.67b16db2eb8d62ccf81254640da64de2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4917977ae9c694b06cf97f1a9ee0cb29",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "ef4db8d3994a52653651",
+    "url": "/react-todo-app/static/css/main.35f14bad.chunk.css"
+  },
+  {
+    "revision": "929e3998fbd4d0ae9576",
+    "url": "/react-todo-app/static/js/2.57fa9df8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.57fa9df8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef4db8d3994a52653651",
+    "url": "/react-todo-app/static/js/main.0816c371.chunk.js"
+  },
+  {
+    "revision": "72ce146e0c780a291f51",
+    "url": "/react-todo-app/static/js/runtime-main.a599ea1a.js"
+  }
+]);
